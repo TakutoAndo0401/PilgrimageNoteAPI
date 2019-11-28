293d7f896b2dfbbdf93e4fe6a84de52521b3c7bc
RitaChan0401 <terminus_rita@yahoo.co.jp>
2019-11-28 18:19:48 +0900

    fix articles tabel

--- database/migrations/2019_11_23_124915_create_articles_table.php
+++ database/migrations/2019_11_23_124915_create_articles_table.php
@@ -14,7 +14,7 @@ class CreateArticlesTable extends Migration
     public function up()
     {
         Schema::create('articles', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('id')->autoIncrement();
             $table->text('image');
             $table->text('title');
             $table->text('date');
